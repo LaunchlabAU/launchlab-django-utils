a63fa8bb81691e1db6d238ab96a6700ef3c3199d
David Pretty <david@launchlab.com.au>
2016-08-24 16:20:20 +1000

    Add UUIDTimestampedModel

--- launchlab_django_utils/models.py
+++ launchlab_django_utils/models.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import uuid
+
+from django.db import models
+
+
+class UUIDTimestampedModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    update_timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
